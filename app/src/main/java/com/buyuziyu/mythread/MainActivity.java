8faa4befecd67a2adb31469fdab8c7085398a417
buyuziyu <buyuziyu@outlook.com>
2016-09-21 13:37:50 +0800

    Initial commit

---
+++ app/src/main/java/com/buyuziyu/mythread/MainActivity.java
@@ -0,0 +1,61 @@
+package com.buyuziyu.mythread;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends Activity {
+
+    private TextView msg;
+    int i = 0;
+    final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    update();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+        void update() {
+            //刷新msg的内容
+            i++;
+            msg.setText("你好！" + i);
+        }
+    };
+    Timer timer = new Timer();
+    TimerTask task = new TimerTask() {
+        public void run() {
+            Message message = new Message();
+            message.what = 1;
+            handler.sendMessage(message);
+        }
+    };
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        msg = (TextView) findViewById(R.id.mText);
+        msg.setText("你好啊！");
+        timer.schedule(task, 1000 * 1, 100 * 1); //启动timer
+    }
+    @Override
+    protected void onDestroy() {
+        if (timer != null) {// 停止timer
+            timer.cancel();
+            timer = null;
+        }
+        super.onDestroy();
+    }
+}
